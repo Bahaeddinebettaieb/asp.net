7d83ae97caee27051af27de9073810b8067580b1
baha.bt <baha.1996.bt@gmail.com>
2022-02-06 13:25:50 +0100

    first commit

---
+++ WebApplication2/Data/CovidContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class CovidContext : DbContext
+    {
+        public CovidContext (DbContextOptions<CovidContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication2.Models.User> User { get; set; }
+    }
+}
